bff5d90000e25d0a063e6e57e051676bd7693257
Victor Skinderviken <159253972+VictorSkinderviken@users.noreply.github.com>
2026-07-13 23:01:09 +0200

    Update wallpaper configuration and touchpad settings

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -219,12 +219,12 @@ hl.config({
 ----------------
 
 -- Standard wallpaper, commented out since we use Hyprpaper
--- hl.config({
---     misc = {
---         force_default_wallpaper = -1,    -- Set to 0 or 1 to disable the anime mascot wallpapers
---         disable_hyprland_logo   = false, -- If true disables the random hyprland logo / anime girl background. :(
---     },
--- })
+hl.config({
+    misc = {
+        force_default_wallpaper = 0,    -- Set to 0 or 1 to disable the anime mascot wallpapers
+        disable_hyprland_logo   = true, -- If true disables the random hyprland logo / anime girl background. :(
+    },
+})
 
 ---------------
 ---- INPUT ----
@@ -243,7 +243,7 @@ hl.config({
         sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
 
         touchpad = {
-            natural_scroll = false,
+            natural_scroll = true,
         },
     },
 })
